c4018877808398afbd2bbfe5fe484252b226fcdf
slow-codex <arnabduttasocial@gmail.com>
2023-03-12 08:19:41 +0530

    Donate Contract Added

--- contracts/Donate.sol
+++ contracts/Donate.sol
@@ -1,5 +1,3 @@
-
-
 /// @title  Contract of the main funding of the project
 /// @author slow-codex
 /// @notice Get and Withdraw Funds and Setting minimum funding
@@ -14,21 +12,34 @@ import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 contract FundMe {
     uint256 public minimumINR = 1;
 
-    function fund()  public payable{
-       // Send ETH
-       require(msg.value > 1e18, "Didn't Send Enough");
-
+    function fund() public payable {
+        // Send ETH
+        require(msg.value > 1e18, "Didn't Send Enough");
     }
 
-    function getPrice() public {
+    function getPrice() public view returns (uint256) {
         // ABI
         // Address - 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
-        
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        );
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return uint256(price * 1e10);
     }
 
-    function getVersion() public view returns(uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(
+            0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        );
         return priceFeed.version();
+    }
+
+    function getConversionRate(
+        uint256 ethAmount
+    ) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUSD = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUSD;
     }
 
     // function withdraw() {
