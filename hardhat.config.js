83d49fe9f1ef36a5c3543c062262433c4e35bf73
slow-codex <arnabduttasocial@gmail.com>
2023-04-04 13:59:11 +0530

    Hardhat config and deploy scripts

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,8 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: "0.8.8",
+  networks: {
+    defaultNetwork: "hardhat",
+  },
 };
